9f1fd7a5a7ddaf54ef3d5917547e4514c44a2788
esitrap <esitrap@gmail.com>
2019-08-11 15:16:05 +0430

    In Work 980520

--- primeng980513/src/app/customers/customer.service.ts
+++ primeng980513/src/app/customers/customer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient } from '@angular/common/http'
 import { Customer } from './customer';
 @Injectable({
   providedIn: 'root'
@@ -7,5 +7,5 @@ import { Customer } from './customer';
 export class CustomerService {
 private customerUrl :'api/customer'
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 }
